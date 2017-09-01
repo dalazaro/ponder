b33d33d7f5c89b534304622d8e3d9606784e47cf
dalazaro <daryl.lazaro@gmail.com>
2017-09-01 15:24:36 -0700

    created an endpoint for hitting the API document

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "splash#index"
+  root to: "application#splash"
+
+  get "/api", to: "application#api"
 
   get "/signup", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
