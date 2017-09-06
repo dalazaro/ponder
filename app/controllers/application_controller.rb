3eda9ad536be1ed543b357029e64635967833acf
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-09-05 17:21:21 -0700

    deleted extra spaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include SessionsHelper
-  
+
   # method for splash page
   def splash
     render('../views/splash')
